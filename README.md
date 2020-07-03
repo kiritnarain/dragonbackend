733fa263ad8ec73de9838ed13350d04273262e30
kiritnarain <kiritnarain@gmail.com>
2020-07-03 21:55:14 +0530

    Fixing ip fetching issue

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # dragonbackend
 Express backend for Dragon Robot  
-Start using: node server.js
+Start using: node server.js  
+Runs on port 5091
--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const port = 5091;
 console.log('Dragonbackend initializing');
 
 app.get('/', (req, res) => {
-    res.send(`Dragon Backend Running! (captured ip: ${req.ip})`)
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    res.send(`Dragon Backend Running! (captured ip: ${ip})`)
 });
 
 app.listen(port, () => {
